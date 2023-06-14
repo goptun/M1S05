7ea4e9c8136b5cc35b6fbc883a210da04d92b829
goptun <ramos.matheus@icloud.com>
2023-06-14 19:07:05 -0300

    Exercício 4

--- exercicio4.js
+++ exercicio4.js
@@ -6,3 +6,15 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+
+var numero = 999;
+var numeroAnterior = 0;
+var numeroAtual = 1;
+var auxiliar;
+
+while (numeroAtual <= numero) {
+  console.log(numeroAtual);
+  auxiliar = numeroAtual;
+  numeroAtual = numeroAnterior + numeroAtual;
+  numeroAnterior = auxiliar;
+}
