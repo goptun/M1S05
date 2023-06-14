a8f61e629f9e08e19d4b3ff7c011c2a7b1b5119f
goptun <ramos.matheus@icloud.com>
2023-06-14 00:12:30 -0300

    Exercício 2

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,33 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+var color = "cian";
+
+var corTraduzida;
+
+switch (color) {
+  case "red":
+    corTraduzida = "Vermelho";
+    break;
+  case "blue":
+    corTraduzida = "Azul";
+    break;
+  case "green":
+    corTraduzida = "Verde";
+    break;
+  case "yellow":
+    corTraduzida = "Amarelo";
+    break;
+  case "black":
+    corTraduzida = "Preto";
+    break;
+  case "white":
+    corTraduzida = "Branco";
+    break;
+  default:
+    corTraduzida = "Não sei";
+    break;
+}
+
+console.log("Tradução: " + corTraduzida);
