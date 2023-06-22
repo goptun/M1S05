93b86e0fd4cc756a60eca932063c663a0a319da0
goptun <ramos.matheus@icloud.com>
2023-06-22 19:59:35 -0300

    Exercício 8

--- exercicio8.js
+++ exercicio8.js
@@ -5,3 +5,28 @@
  * matemática. A função `calculadora` executa a operação matemática nos dois números fornecidos e retorna o resultado.
  * Testa a função utilizando diferentes operações como adição, subtração, multiplicação e divisão.
  */
+
+function calculadora(num1, num2, operacao) {
+  return operacao(num1, num2);
+}
+
+function adicao(num1, num2) {
+  return num1 + num2;
+}
+
+function subtracao(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplicacao(num1, num2) {
+  return num1 * num2;
+}
+
+function divisao(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(calculadora(2, 3, adicao));
+console.log(calculadora(2, 3, subtracao));
+console.log(calculadora(2, 3, multiplicacao));
+console.log(calculadora(2, 3, divisao));
