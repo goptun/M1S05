88d3cb0463cc7ba07dd5adacab4f59a790f02c71
goptun <ramos.matheus@icloud.com>
2023-06-22 19:51:28 -0300

    Exercício 6

--- exercicio6.js
+++ exercicio6.js
@@ -5,3 +5,17 @@
  * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+
+let produtos = ["Arroz", "Feijão", "Carne", "Verduras", "Frutas"];
+let precos = [2.99, 3.99, 15.0, 2.5, 1.99];
+let totalProdutos = 0;
+let totalValor = 0;
+
+for (let i = 0; i < produtos.length; i++) {
+  console.log(`${produtos[i]} - ${precos[i]}`);
+  totalProdutos++;
+  totalValor += precos[i];
+}
+
+console.log(`Total de produtos: ${totalProdutos}`);
+console.log(`Valor total: ${totalValor}`);
